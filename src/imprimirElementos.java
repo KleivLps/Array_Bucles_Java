8f58717994fb4ea72f15ebb32ce519d31f46bdab
kleiver <kleiver1604@gmail.com>
2024-07-13 23:05:37 -0500

    Programa que ayuda a entender la introduccion a los bucles.

---
+++ src/imprimirElementos.java
@@ -0,0 +1,11 @@
+
+public class imprimirElementos {
+    public static void main(String[] args) {
+
+            int[] numeros = {8,10,5,3,4};
+            System.out.println("Los elementos del array son: ");
+            for (int num : numeros) {
+                System.out.println(num);
+            }
+    }
+}
