5a96eab00f8441f809bfe9724d5383feff1d698f
kleiver <kleiver1604@gmail.com>
2024-07-13 23:08:08 -0500

    Programa que imprime los elementos en el orden inverso dentro del array.

---
+++ src/ordenInverso.java
@@ -0,0 +1,11 @@
+public class ordenInverso {
+    public static void main(String[] args) {
+
+        int[] numeros = {8,10,5,3,4};
+        System.out.println("orden inverso: ");
+        for (int i = numeros.length -1; i>= 0; i--) {
+            System.out.println(numeros[i]);
+        }
+
+}
+    }
